bff9fccc752a7ca362a3338edf28803cffb3e436
Anton Davydov <fetsorn@Antons-MacBook-Air.local>
2021-09-04 00:53:07 +0300

    feat: logs with timestamps

--- src/main.rs
+++ src/main.rs
@@ -6,17 +6,33 @@ mod handlers;
 mod models;
 
 use crate::dialogue::Dialogue;
+use std::io::Write;
+use chrono::Local;
+use env_logger::Builder;
+use log::LevelFilter;
 
 const URL: &str = "https://forum.graviton.one";
 // pub const URL: &str = "https://try.discourse.org";
 
 #[tokio::main]
 async fn main() {
+    Builder::new()
+        .format(|buf, record| {
+            writeln!(buf,
+                     "{} [{}] - {}",
+                     Local::now().format("%Y-%m-%dT%H:%M:%S"),
+                     record.level(),
+                     record.args()
+            )
+        })
+        .filter(None, LevelFilter::Info)
+        .init();
+
     run().await;
 }
 
 async fn run() {
-    teloxide::enable_logging!();
+    // teloxide::enable_logging!();
     log::info!("Starting dialogue_bot...");
 
     let bot = Bot::from_env().auto_send();
--- src/models/error.rs
+++ src/models/error.rs
@@ -2,8 +2,6 @@ use thiserror::Error;
 
 #[derive(Error, Debug)]
 pub enum BotError {
-    #[error("rate limit")]
-    RateLimit(),
     #[error("parse failed")]
     Parse(#[from] serde_json::Error),
     #[error("request failed")]
